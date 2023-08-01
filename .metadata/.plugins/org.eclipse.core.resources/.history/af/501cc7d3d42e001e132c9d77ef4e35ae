2a0c1c86293ba951e70a7a72454d6120dbccacba
pragadeesh <pragadeeshg84@gmail.com>
2023-08-01 20:12:11 +0530

    Creating Signed-off-by: pragadeesh <pragadeeshg84@gmail.com>

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/af/501cc7d3d42e001e132c9d77ef4e35ae
@@ -0,0 +1,9 @@
+package com.test1;
+
+public class Color {
+
+	public static void main(String[] args) {
+		System.out.println("Green");
+	}
+
+}
---
+++ Java_30_3/src/com/Sample1/Plant.java
@@ -0,0 +1,9 @@
+package com.Sample1;
+
+public class Plant {
+
+	public static void main(String[] args) {
+    System.out.println("Vallarai");
+	}
+
+}
