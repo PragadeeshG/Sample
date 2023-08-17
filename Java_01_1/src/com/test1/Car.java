6c8430e13d61db3ee8d635656c51453cf84c022d
pragadeesh <pragadeeshg84@gmail.com>
2023-08-17 12:51:27 +0530

    Starting Signed-off-by: pragadeesh <pragadeeshg84@gmail.com>

--- Java_01_1/src/com/test1/Car.java
+++
@@ -1,9 +0,0 @@
-package com.test1;
-
-public class Car {
-
-	public static void main(String[] args) {
-    System.out.println("Swift");
-	}
-
-}
--- Java_30_3/src/com/Sample1/Plant.java
+++
@@ -1,9 +0,0 @@
-package com.Sample1;
-
-public class Plant {
-
-	public static void main(String[] args) {
-    System.out.println("Vallarai");
-	}
-
-}
